5774ffe08c640eab0b97df201f86745fe547c636
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2024-10-21 22:35:16 +0200

    Add files via upload

---
+++ K -NN.R
@@ -0,0 +1,69 @@
+# K-Nearest Neighbors (K-NN)
+
+# Importing the dataset
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[3:5]
+
+# Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+#Podany przez Ciebie kod jest używany do konwersji zmiennej o nazwie "Purchased" w zbiorze danych o nazwie "dataset" na zmienną czynnikową (factor) z poziomami 0 i 1.
+#W R, zmienna czynnikowa jest używana do reprezentowania danych kategorycznych. Domyślnie R traktuje każdą zmienną o niewielkiej liczbie unikalnych wartości jako zmienną czynnikową. Jednak czasami warto ręcznie określić poziomy zmiennej czynnikowej, szczególnie gdy mamy do czynienia z danymi binarnymi (tzn. danymi, które mogą przyjmować tylko dwie wartości).
+#Funkcja "factor" jest używana do tworzenia zmiennej czynnikowej w R. W tym kodzie funkcja "factor" jest stosowana do zmiennej "Purchased" w zbiorze danych "dataset". Argument "levels" służy do określenia poziomów zmiennej czynnikowej. Tutaj poziomy są ustawione na 0 i 1.
+#Ustawienie poziomów zmiennej czynnikowej zapewnia, że poziomy są reprezentowane w sposób spójny przez cały proces analizy danych. Na przykład, jeśli chcemy utworzyć wykres lub tabelę zmiennej "Purchased", R będzie używał poziomów określonych w tym kodzie do reprezentacji danych.
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+# Fitting K-NN to the Training set and Predicting the Test set results
+library(class)
+y_pred = knn(train = training_set[, -3],
+             test = test_set[, -3],
+             cl = training_set[, 3],
+             k = 5,
+             prob = TRUE)
+y_pred
+
+# Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+cm 
+
+# Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
+plot(set[, -3],
+     main = 'K-NN (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
+plot(set[, -3],
+     main = 'K-NN (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
